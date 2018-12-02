8c4c13d615eaa37bca023854173dc26f02c95706
cipepser <respepic@gmail.com>
2018-12-02 17:17:01 +0900

    fix a bug to use byte slice instead of string.

--- decoder/decoder_test.go
+++ decoder/decoder_test.go
@@ -1,7 +1,6 @@
 package decoder
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -12,7 +11,7 @@ func TestUnmarshalPerson(t *testing.T) {
 		expect Person
 	}{
 		{
-			b: []byte("0a070a05416c69636512020814"),
+			b: []byte{10, 7, 10, 5, 65, 108, 105, 99, 101, 18, 2, 8, 20}, // "0a070a05416c69636512020814"
 			expect: Person{
 				Name: &Name{
 					Value: "Alice",
@@ -25,8 +24,11 @@ func TestUnmarshalPerson(t *testing.T) {
 	}
 
 	for i, tt := range tests {
-		p := Person{}.Unmarshal(tt.b)
-		if reflect.DeepEqual(p, tt.expect) {
+		p := Person{}
+		if err := p.Unmarshal(tt.b); err != nil {
+			t.Fatalf("test[%d - failed to Unmarshal. got err:%q", i, err)
+		}
+		if p.Name.Value != tt.expect.Name.Value || p.Age.Value != tt.expect.Age.Value {
 			t.Fatalf("test[%d - failed to Unmarshal. expected=%q, got=%q", i, tt.expect, p)
 		}
 	}
