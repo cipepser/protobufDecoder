3e59ad0e669364d0f2fb4300d372ee9944447c54
cipepser <respepic@gmail.com>
2018-12-04 20:45:03 +0900

    add tests for zero value

--- decoder/decoder_test.go
+++ decoder/decoder_test.go
@@ -3,27 +3,41 @@ package decoder
 import (
 	"encoding/hex"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestUnmarshalPerson(t *testing.T) {
+func atob(s string) []byte {
+	b, _ := hex.DecodeString(s)
+	return b
+}
 
+func TestUnmarshalPerson(t *testing.T) {
 	tests := []struct {
 		b      []byte
 		expect Person
 	}{
 		{
-			//b: []byte{10, 7, 10, 5, 65, 108, 105, 99, 101, 18, 2, 8, 20}, // "0a070a05416c69636512020814"
-			b: func(s string) []byte {
-				b, _ := hex.DecodeString(s)
-				return b
-			}("0a070a05416c69636512020814"),
+			b: atob("0a070a05416c69636512020814"),
 			expect: Person{
-				Name: &Name{
-					Value: "Alice",
-				},
-				Age: &Age{
-					Value: 20,
-				},
+				Name: &Name{Value: "Alice"},
+				Age:  &Age{Value: 20},
+			},
+		},
+		{
+			b:      atob(""),
+			expect: Person{},
+		},
+		{
+			b: atob("0a070a05416c696365"),
+			expect: Person{
+				Name: &Name{Value: "Alice"},
+			},
+		},
+		{
+			b: atob("12020814"),
+			expect: Person{
+				Age:  &Age{Value: 20},
 			},
 		},
 	}
@@ -33,7 +47,7 @@ func TestUnmarshalPerson(t *testing.T) {
 		if err := p.Unmarshal(tt.b); err != nil {
 			t.Fatalf("test[%d - failed to Unmarshal. got err:%q", i, err)
 		}
-		if p.Name.Value != tt.expect.Name.Value || p.Age.Value != tt.expect.Age.Value {
+		if diff := cmp.Diff(p, tt.expect); diff != "" {
 			t.Fatalf("test[%d - failed to Unmarshal. expected=%q, got=%q", i, tt.expect, p)
 		}
 	}
